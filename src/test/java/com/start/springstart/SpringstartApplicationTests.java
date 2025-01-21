6d774a6685af4603ef6f921f0f2ff3102a9e8ee3
roman8128 <stems_notice_0i@icloud.com>
2025-01-21 17:20:52 +0100

    initial commit

---
+++ src/test/java/com/start/springstart/SpringstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.start.springstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
