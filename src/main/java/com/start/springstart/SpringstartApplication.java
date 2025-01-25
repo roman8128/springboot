f5a1eb42a2ce785796ab9bb39533035426d3b6cb
Roman <4464530+roman8128@users.noreply.github.com>
2025-01-25 16:31:56 +0100

    Added logging and showing printer colors via console, removed controller

--- src/main/java/com/start/springstart/SpringstartApplication.java
+++ src/main/java/com/start/springstart/SpringstartApplication.java
@@ -1,13 +1,26 @@
 package com.start.springstart;
 
+import com.start.springstart.services.ColorPrinter;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log
 @SpringBootApplication
-public class SpringstartApplication {
+public class SpringstartApplication implements CommandLineRunner {
+	private final ColorPrinter colorPrinter;
+
+	public SpringstartApplication(ColorPrinter colorPrinter) {
+		this.colorPrinter = colorPrinter;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstartApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(colorPrinter.print());
+	}
 }
